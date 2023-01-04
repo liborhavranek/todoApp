f0e0025826a1d7205ed575594e279cef07aee372
liborhavranek <liborhavranek91@gmail.com>
2023-01-04 21:57:45 +0100

    save task from form working excelent

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, response
-from .models import task
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    tasks = task.objects.all()
+    tasks = Task.objects.all()
+    if request.method == 'POST':
+        title = request.POST['title']
+        desc = request.POST['desc']
+        due = request.POST['due']
+        task = Task(title=title, desc=desc, due=due)
+        task.save()
     return render(request, 'index.html', {"tasks":tasks})
