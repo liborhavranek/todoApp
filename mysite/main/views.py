1cc5d30faa1547707f745cff0fd14c6eb3501595
liborhavranek <liborhavranek91@gmail.com>
2023-01-04 23:33:59 +0100

    page for each task created

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, response
 from .models import Task
 from django.contrib import messages
@@ -12,7 +12,17 @@ def index(request):
         title = request.POST['title']
         desc = request.POST['desc']
         due = request.POST['due']
-        task = Task(title=title, desc=desc, due=due)
-        task.save()
-        messages.add_message(request, messages.SUCCESS, 'Task created successfully!')
+        if len(title) < 3:
+            messages.error(request,'Title must be longer than 3 characters long')
+        elif len(desc) < 10: 
+            messages.error(request,'Description must be longer than 10 characters long')
+            return redirect('index')
+        else:
+            task = Task(title=title, desc=desc, due=due)
+            task.save()
+            messages.add_message(request, messages.SUCCESS, 'Task created successfully!')
     return render(request, 'index.html', {"tasks":tasks})
+
+def task(request, id):
+    task = Task.objects.get(id=id)
+    return render(request, 'task.html', {"task":task})
