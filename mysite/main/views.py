7c9a2838f2d1a1e977d86a6a6828ac2474cc032b
liborhavranek <liborhavranek91@gmail.com>
2023-01-04 22:33:38 +0100

    message is showed

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, response
 from .models import Task
+from django.contrib import messages
+import datetime
 
 # Create your views here.
 
@@ -12,4 +14,5 @@ def index(request):
         due = request.POST['due']
         task = Task(title=title, desc=desc, due=due)
         task.save()
+        messages.add_message(request, messages.SUCCESS, 'Task created successfully!')
     return render(request, 'index.html', {"tasks":tasks})
